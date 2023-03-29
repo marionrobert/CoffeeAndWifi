a1e1529f6e44b595d58a971e683410823a1670d8
Marion <mrobert148@gmail.com>
2023-03-29 16:37:21 +0200

    display all cafes in a bootstrap table

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import csv
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ["secret_key"]
 Bootstrap(app)
 
 
@@ -42,11 +43,12 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='') as csv_file:
+    with open('cafe-data.csv', newline='', encoding="utf8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
+        print(list_of_rows)
     return render_template('cafes.html', cafes=list_of_rows)
 
 
